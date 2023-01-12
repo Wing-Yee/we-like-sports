2bd7bc566eb5f5ba23abe7ac67858253c1144bf5
Wing-Yee <110760706+Wing-Yee@users.noreply.github.com>
2023-01-12 21:30:26 +0800

    files for fastapi and streamlit

---
+++ api/fast.py
@@ -0,0 +1,74 @@
+#each time you want to run from your local terminal
+
+#pyenv virtualenv 3.8.12  welikesports-env
+#pip install --upgrade pip
+#pyenv local welikesports-env
+#code .
+
+#in new tab of terminal
+# python -m pip install fastapi
+# pip install uvicorn
+
+#uvicorn sports_api.api.fast:app --reload
+
+#import pandas as pd
+#from datetime import datetime
+
+from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
+import streamlit as st
+import json
+import requests
+
+app = FastAPI()
+
+@app.get("/")
+def root():
+    return {'greeting': 'Hello'}
+
+@app.get('/input')
+def input():
+    st.title("We Like Sports Recommendation")
+    st.write("")
+    st.write("Select the importance of the below features from 1-5")
+    st.write("1 being least important, 5 being most important")
+
+    sentiment = st.slider("Sentiment", 1,5,1)
+    coverage = st.slider("Coverage", 1,5,1)
+    engagement = st.slider("Engagement", 1,5,1)
+
+#https://medium.com/codex/streamlit-fastapi-%EF%B8%8F-the-ingredients-you-need-for-your-next-data-science-recipe-ffbeb5f76a92
+
+@app.get("/predict")
+def predict(sentiment: st.slider("Sentiment", 1,5,1),
+            coverage: int,
+            engagement: int):
+
+    sentiment = st.slider("Sentiment", 1,5,1)
+    coverage = st.slider("Coverage", 1,5,1)
+    engagement = st.slider("Engagement", 1,5,1)
+
+    #convert params into X_pred dictionary format
+    X_pred_conv = pd.DataFrame(dict(
+            # key=[str(pickup_datetime.strftime('%Y-%m-%d %H:%M:%S UTC'))],
+            #What to put for key?????
+            sentiment=[int(sentiment)],
+            coverage=[int(coverage)],
+            engagement=[int(engagement)]
+            ))
+
+    #predict y using X_pred
+    #from taxifare.interface.main import pred
+    #predict = float(pred(X_pred_conv))
+
+    #convert prediction into dictionary from numpy array
+    # predict_dict = dict(enumerate(predict.flatten(), 1))
+    #return {'fare_amount' : predict }
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],  # Allows all origins
+    allow_credentials=True,
+    allow_methods=["*"],  # Allows all methods
+    allow_headers=["*"],  # Allows all headers
+)
---
+++ api/streamlit_name.py
@@ -0,0 +1,21 @@
+import streamlit as st
+import numpy as np #use to convert image
+import json #for when we have real results
+import requests #for when we have real results
+from PIL import Image
+
+test_image = Image.open('wordcloudtest.jpg')
+
+st.title("We Like Sports Recommendation")
+st.write("")
+st.write("Select the importance of the below features from 1-5")
+st.write("1 being least important, 5 being most important")
+
+sentiment = st.slider("Sentiment", 1,5,1)
+coverage = st.slider("Coverage", 1,5,1)
+engagement = st.slider("Engagement", 1,5,1)
+
+if st.button('Calculate'):
+    #res = requests.post(url = http://localhost:8000/calculate, data = json.dumps(X_pred_conv) )
+    st.subheader(f"Your recommended Sport event is...The EFL ") #{res.text} ")
+    st.image(test_image, caption='Top used words for this event')
