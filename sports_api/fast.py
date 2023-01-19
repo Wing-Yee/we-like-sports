dee88d169c4b6eee22289cbb1bb42c9f732cd04d
WingYee Lau <wing-yee@hotmail.co.uk>
2023-01-19 19:51:17 +0800

    updated streamlit to chose event name image, added instructions for running fastpy and streamlit locally

---
+++ sports_api/fast.py
@@ -0,0 +1,73 @@
+##the first time you run you need to create the virtual environment
+# cd into the we-like-sports folder in your terminal
+# pyenv virtualenv 3.8.12  .env
+## then in VS code, command + p and type 'python select interpretor'
+## choose the env you just made!
+
+##each time you want to run from your local terminal
+
+#pyenv virtualenv 3.8.12  we-like-sports-env
+#pip install --upgrade pip
+#pip install streamlit
+#pyenv local we-like-sports-env
+#code .
+
+##in new tab of terminal
+# python -m pip install fastapi
+# pip install uvicorn
+
+#uvicorn sports_api.fast:app --reload
+
+import streamlit as st
+from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
+
+
+app = FastAPI()
+
+@app.get("/")
+def root():
+    return {'greeting': 'Hello'}
+
+#https://medium.com/codex/streamlit-fastapi-%EF%B8%8F-the-ingredients-you-need-for-your-next-data-science-recipe-ffbeb5f76a92
+
+
+
+def calculate(sentiment: st.slider("Sentiment", 1,5,1),
+            coverage: int,
+            likes: int,
+            comments: int,
+            retweets: int):
+
+    sentiment = st.slider("Sentiment", 1,5,1)
+    coverage = st.slider("Coverage", 1,5,1)
+    likes = st.slider("Likes", 1,5,1)
+    comments = st.slider("Comments", 1,5,1)
+    retweets = st.slider("Retweets", 1,5,1)
+
+    #convert params into X_pred dictionary format
+    X_pred_conv = pd.DataFrame(dict(
+            # key=[str(pickup_datetime.strftime('%Y-%m-%d %H:%M:%S UTC'))],
+            #What to put for key?????
+            sentiment=[int(sentiment)],
+            coverage=[int(coverage)],
+            likes=[int(likes)],
+            comments=[int(comments)],
+            retweets=[int(retweets)],
+            ))
+
+    #predict y using X_pred
+    #from taxifare.interface.main import pred
+    #predict = float(pred(X_pred_conv))
+
+    #convert prediction into dictionary from numpy array
+    # predict_dict = dict(enumerate(predict.flatten(), 1))
+    return {'fare_amount' : 'hold' }
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],  # Allows all origins
+    allow_credentials=True,
+    allow_methods=["*"],  # Allows all methods
+    allow_headers=["*"],  # Allows all headers
+)
---
+++ sports_api/streamlit_name.py
@@ -0,0 +1,50 @@
+##once youve loaded fastpy open another tab
+# streamlit run stream_lit.py
+
+import streamlit as st
+import numpy as np #use to convert image
+import json #for when we have real results
+import requests #for when we have real results
+from PIL import Image
+import pandas as pd
+import sys, os
+
+
+
+##Locate images and csvs from folder and open
+event_name = 'Rainbow Laces' + '.png' ##this should be replaced by our method from the user inputs
+word_cloud = Image.open(event_name)
+
+event_icon = Image.open('EFL.png')
+result_table = pd.read_csv('test_results_table.csv',  encoding= 'unicode_escape')
+comparison_table = pd.read_csv('test_results_table.csv',  encoding= 'unicode_escape')
+
+# #UI for user input
+st.title("We Like Sports Recommendation")
+st.write("[![Star](https://img.shields.io/github/stars/Wing-Yee/we-like-sports.svg?logo=github&style=social)](https://gitHub.com/Wing-Yee/we-like-sports)")
+st.write("")
+st.subheader("Rate the below dimensions based on their importance from 1-5")
+st.subheader("1 being least important, 5 being most important")
+
+sentiment = st.slider("Sentiment", 1,5,1)
+coverage = st.slider("Coverage", 1,5,1)
+likes = st.slider("Likes", 1,5,1)
+comments = st.slider("Comments", 1,5,1)
+retweets = st.slider("Retweets", 1,5,1)
+
+# #UI for output
+if st.button('Calculate'):
+    #res = requests.post(url = http://localhost:8000/calculate, data = json.dumps(X_pred_conv) )
+    st.subheader(f"Your recommended Sport event is...The EFL ") #{res.text} ")
+
+    st.write(f"Event Metrics")
+    st.dataframe(result_table)
+
+    st.write(f"Comparisons Event Metrics")
+    st.dataframe(comparison_table)
+
+    col1, col2 = st.columns((2,1))
+    with col1:
+       st.image(word_cloud, caption='Top used words for this event')
+    with col2:
+       st.image(event_icon)
