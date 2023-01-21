720d242e3acfc85b7b2a562403f74eac927eb08d
WingYee Lau <wing-yee@hotmail.co.uk>
2023-01-21 17:28:47 +0800

    api changes

--- sports_api/fast.py
+++ sports_api/fast.py
@@ -18,27 +18,26 @@
 
 #uvicorn sports_api.fast:app --reload
 
+import pandas as pd
 import streamlit as st
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
 
+#results dataframe
+df2 = pd.read_csv('sentiment_analysis_score.csv')
+df2 = df2.drop(columns = 'Sentiment')
 
 app = FastAPI()
 
-@app.get("/")
-def root():
-    return {'greeting': 'Hello'}
-
-# @app.get('/input')
-
 #https://medium.com/codex/streamlit-fastapi-%EF%B8%8F-the-ingredients-you-need-for-your-next-data-science-recipe-ffbeb5f76a92
 
-# @app.get("/calculate")
-def calculate(sentiment: st.slider("Sentiment", 1,5,1),
-            coverage: int,
-            likes: int,
-            comments: int,
-            retweets: int):
+@app.get("calculate")
+def calculate(sentiment: st.slider("Sentiment", 0,5,1),
+            coverage: st.slider("Coverage", 0,5,1),
+            likes: st.slider("Likes", 0,5,1),
+            comments: st.slider("Comments", 0,5,1),
+            retweets: st.slider("Retweets", 0,5,1)):
+
 
     sentiment = st.slider("Sentiment", 0,5,1)
     coverage = st.slider("Coverage", 0,5,1)
@@ -46,29 +45,45 @@ def calculate(sentiment: st.slider("Sentiment", 1,5,1),
     comments = st.slider("Comments", 0,5,1)
     retweets = st.slider("Retweets", 0,5,1)
 
-    #convert params into X_pred dictionary format
-    X_pred_conv = pd.DataFrame(dict(
-            # key=[str(pickup_datetime.strftime('%Y-%m-%d %H:%M:%S UTC'))],
-            #What to put for key?????
-            sentiment=[int(sentiment)],
-            coverage=[int(coverage)],
-            likes=[int(likes)],
-            comments=[int(comments)],
-            retweets=[int(retweets)],
-            ))
+#     #convert params dictionary format
+#     user_input_df = pd.DataFrame(dict(
+#             sentiment=[int(sentiment)],
+#             coverage=[int(coverage)],
+#             likes=[int(likes)],
+#             comments=[int(comments)],
+#             retweets=[int(retweets)]
+#             ))
 
-    #predict y using X_pred
-    #from taxifare.interface.main import pred
-    #predict = float(pred(X_pred_conv))
+#     return {'test' : user_input_df }
 
-    #convert prediction into dictionary from numpy array
-    # predict_dict = dict(enumerate(predict.flatten(), 1))
-    return {'fare_amount' : 'hold' }
+#     output_name = select_event(user_input_df['comments'],
+#             user_input_df['likes'],
+#             user_input_df['retweets'],
+#             user_input_df['sentiment'],
+#             user_input_df['coverage'])
 
-app.add_middleware(
-    CORSMiddleware,
-    allow_origins=["*"],  # Allows all origins
-    allow_credentials=True,
-    allow_methods=["*"],  # Allows all methods
-    allow_headers=["*"],  # Allows all headers
-)
+#     return output_name
+
+
+# def select_event(comments_score, likes_score, retweet_score, compound_score, coverage_score):
+#     df2['comments_weighted'] = df2['Comments'].apply(lambda x: x*comments_score)
+
+#     df2['likes_weighted'] = df2['Likes'].apply(lambda x: x*likes_score)
+
+#     df2['retweet_weighted'] = df2['Retweets'].apply(lambda x: x*retweet_score)
+
+#     df2['compound_weighted'] = df2['Compound'].apply(lambda x: x*compound_score)
+
+#     df2['coverage_weighted'] = df2['Tidy_tweets'].apply(lambda x: x*coverage_score)
+
+#     df2['final_weighted'] = df2['comments_weighted'] + df2['likes_weighted'] + df2['retweet_weighted'] + df2['compound_weighted'] + df2['coverage_weighted']
+
+#     return df2.iloc[df2.final_weighted.argmax(),0]
+
+
+# app.add_middleware(
+#     CORSMiddleware,
+#     allow_origins=["*"],  # Allows all origins
+#     allow_credentials=True,
+#     allow_methods=["*"],  # Allows all methods
+#     allow_headers=["*"],  # Allows all headers
