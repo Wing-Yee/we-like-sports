c2ace2fa9e08a2abc5e9fa519fa8b13a908afa0d
WingYee Lau <wing-yee@hotmail.co.uk>
2023-02-04 11:27:24 +0800

    changes

--- sports_api/streamlit_name.py
+++ sports_api/streamlit_name.py
@@ -9,16 +9,20 @@ from PIL import Image
 import pandas as pd
 import sys, os
 
-#import fast.calculate
+#from sentiment_analysis_test import select_event
+
+#path to csv file
+
+abs_csv_path = os.path.join(os.path.dirname(__file__), '../sports_api/sentiment_analysis_score.csv')
+result_table = pd.read_csv(abs_csv_path,  encoding= 'unicode_escape')
 
 
 ##Locate images and csvs from folder and open
 event_name = '../sports_api/' + 'Rainbow Laces' + '.png' ##this should be replaced by our method from the user inputs
 word_cloud = Image.open(event_name)
-
 event_icon = Image.open('../sports_api/EFL.png')
-result_table = pd.read_csv('../sports_api/test_results_table.csv',  encoding= 'unicode_escape')
-comparison_table = pd.read_csv('../sports_api/test_results_table.csv',  encoding= 'unicode_escape')
+
+
 
 # #UI for user input
 st.title("We Like Sports Recommendation")
@@ -27,25 +31,39 @@ st.write("")
 st.subheader("Rate the below dimensions based on their importance from 0-5")
 st.subheader("0 being least important, 5 being most important")
 
+#slide inputs
 sentiment = st.slider("Sentiment", 0,5,1)
 coverage = st.slider("Coverage", 0,5,1)
 likes = st.slider("Likes", 0,5,1)
 comments = st.slider("Comments", 0,5,1)
 retweets = st.slider("Retweets", 0,5,1)
 
-output_name = fast.calculate(sentiment,coverage, likes, comments, retweets)
 
+#?sentiment=1&coverage=1&likes=1&comments=1&retweets=1
 
 # #UI for output
 if st.button('Calculate'):
-    #res = requests.post(url = http://localhost:8000/calculate, data = json.dumps(X_pred_conv) )
-    st.subheader(f"Your recommended Sport event is... {output_name} ") #{res.text} ")
+    #make dictionary
+    params = {
+    'sentiment':sentiment,
+    'coverage':coverage,
+    'likes':likes,
+    'comments':comments,
+    'retweets':retweets,
+    }
+
+    url = 'http://127.0.0.1:8009/calculate'
+    response = requests.get(url, params=params)
+    return_result = response.json()
+    result_name = return_result['event name']
+
+    st.subheader(f"Your recommended Sport event is... {result_name} ") #{res.text} ")
 
     st.write(f"Event Metrics")
-    st.dataframe(result_table)
+    st.dataframe(result_table.loc[result_table['source'] == result_name])
 
     st.write(f"Comparisons Event Metrics")
-    st.dataframe(comparison_table)
+    st.dataframe(result_table.loc[result_table['source'] != result_name])
 
     col1, col2 = st.columns((2,1))
     with col1:
